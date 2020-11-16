cf803190f8c66e241d74ef4672f768a7030f44b9
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-11-16 17:43:59 +0100

    Ex. 2.11 done

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ search, handleChange }) => {
     return (
@@ -34,15 +35,20 @@ const Book = ({ persons, search }) => {
 }
 
 const App = () => {
-    const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
+    const [ persons, setPersons ] = useState([]);
     const [ newName, setNewName ] = useState('');
     const [ newNumber, setNewNumber ] = useState('');
     const [ search, setSearch ] = useState('');
+
+    const hook = () => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(res => {
+                setPersons(res.data);
+            });
+    }
+    
+    useEffect(hook, []);
 
     const handleNameChange = (e) => {
         setNewName(e.target.value);
