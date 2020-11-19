e37bf7c0dff95f7cca80435192f8a5e9aa13b9a5
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-11-19 13:32:33 +0100

    Ex. up to 3.5 done

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "2130-431202-1",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end();
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
+
+    res.status(204).end();
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000);
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    if (body.name && body.number) {
+        const person = {
+            "name": body.name,
+            "number": body.number,
+            "id": generateId()
+        }
+        persons = persons.concat(person);
+        res.json(person)
+    } else {
+        res.status(400).end();
+    }
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>${new Date()}`)
+})
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is runnning on port ${PORT}`);
+})
